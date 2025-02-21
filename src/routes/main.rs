41c670673980b0a6efa2718df16bef7ce859c32e
Tom <josephdotson@belkonar.com>
2025-02-20 19:45:21 -0500

    db healthcheck

--- src/routes/main.rs
+++ src/routes/main.rs
@@ -1,9 +1,13 @@
 use crate::{app_state::AppState, models::SimpleResponse};
-use axum::{routing::post, Json, Router};
+use axum::{
+    extract::State,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::Deserialize;
 use tower_http::cors::CorsLayer;
 use uuid::Uuid;
-use whynot_errors::JsonResult;
+use whynot_errors::{json_ok, AppError, JsonResult};
 
 #[derive(Deserialize, Debug)]
 pub struct HandoffRequest {
@@ -16,8 +20,18 @@ async fn handoff(Json(body): Json<HandoffRequest>) -> JsonResult<SimpleResponse>
     SimpleResponse::json(format!("{:?}", body))
 }
 
+async fn db_healthcheck(State(state): State<AppState>) -> JsonResult<SimpleResponse> {
+    let result: (i32,) = sqlx::query_as("SELECT 12;")
+        .fetch_one(&state.db)
+        .await
+        .map_err(AppError::from)?;
+
+    json_ok(SimpleResponse::new(result.0))
+}
+
 pub fn api_routes() -> Router<AppState> {
     Router::new()
         .route("/handoff", post(handoff))
+        .route("/db-healthcheck", get(db_healthcheck))
         .layer(CorsLayer::permissive()) // TODO: fix this lol
 }
