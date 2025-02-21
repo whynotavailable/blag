37c0baf0d761dd9fcc18d07b8ab52b914e16b915
Tom <josephdotson@belkonar.com>
2025-02-21 08:28:13 -0500

    small setup changes

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ use whynot_errors::{SetupError, SetupResult};
 #[tokio::main]
 async fn main() -> SetupResult {
     let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::TRACE)
-        // completes the builder.
+        .with_max_level(Level::DEBUG)
         .finish();
 
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::subscriber::set_global_default(subscriber)
+        .map_err(|err| SetupError::new(format!("setting default subscriber failed: {}", err)))?;
+
     setup().await.map_err(SetupError::new)?;
     Ok(())
 }
